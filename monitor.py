9f7d9f746f872c359b34139172aa49cf1ce4fc7d
Cahit Onur <cahitonur@gmail.com>
2014-10-24 01:11:09 +0530

    stderr

--- monitor.py
+++ monitor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # __author__ = 'cahitonur'
 
-from subprocess import Popen, check_call, CalledProcessError
+from subprocess import Popen, check_output, CalledProcessError, STDOUT
 from postman import send_mail
 import time
 
@@ -11,13 +11,13 @@ receiver = 'you@example.com'
 
 def check_status():
     try:
-        check_call(['lsof', '-i', ':3306'])
+        check_output(['lsof', '-i', ':3306'], stderr=STDOUT)
 
     except CalledProcessError:
 
         Popen(['service', 'mysql', 'start'])
         time.sleep(5)
-        send_mail('MySql Server was down.', 'mail_text', sender, receiver)
+        send_mail('MySql Server was down.', '/root/mysql_monitor/mail_text', sender, receiver)
 
 if __name__ == '__main__':
     check_status()
